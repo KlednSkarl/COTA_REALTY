a0401741b6d64672ad80b7213fde0d68d5500924
KlednSkarl <crissdejose@gmail.com>
2025-08-26 11:25:22 +0800

    Update for Batch upload

--- server.js
+++ server.js
@@ -270,7 +270,7 @@ app.post('/PerBatchUpload', async (req,res) => {
             tvp.columns.add("CAmt", sql.BigInt);
             tvp.columns.add("RefLine", sql.BigInt);
             tvp.columns.add("UserID", sql.VarChar(20));
-
+            tvp.columns.add("Line",sql.BigInt);
 
             req.body.forEach(item => {
                tvp.rows.add(
@@ -286,7 +286,8 @@ app.post('/PerBatchUpload', async (req,res) => {
         safeDate(item.dteDC),
         item.CAmt,
         item.RefLine,
-        item.UserID
+        item.UserID,
+        item.Line
 
 
                ); 
